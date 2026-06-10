80b5e1662f15a046624649477163782acbd66c82
Yosuf20 <jyosuf6@gmail.com>
2026-06-10 17:29:43 +0530

    Added FastAPI frontend integration

--- app.py
+++ app.py
@@ -1,48 +1,44 @@
-import streamlit as st
-import tensorflow as tf
-import pandas as pd
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 from PIL import Image
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
-model = tf.keras.models.load_model("alzheimer_cnn_model.keras")
-st.title("Alzeimer Class Predictions")
+app = FastAPI()
 
+model = tf.keras.models.load_model('alzheimer_cnn_model.keras')
 
-file_image = st.file_uploader("Upload MRI Image", type=["jpg", "png", "jpeg"])
+predic_class = [
+    "Non Demented",
+    "Very Mild Demented",
+    "Mild Demented",
+    "Moderate Demented"
+]
 
-
-with st.expander("Classes"):
-    st.markdown("""
-    1. Non Demented
-    2. Very Mild Demented
-    3. Mild Demented
-    4. Moderate Demented
-    """)
-
-if file_image is not None:
-    image = Image.open(file_image).convert("RGB")
+def Preprocess(image):
     image = image.resize((128, 128))
     image = np.array(image)
     image = image/255.0
     image = np.expand_dims(image, axis=0)
+    return image
 
-    prediction = model.predict(image)
+@app.get('/')
+def show():
+    return {'message' : 'alzheimer prediction testing'}
 
-    classes = [
-        "Mild Demented",
-        "Moderate Demented",
-        "Healthy",
-        "Very Mild Demented"
-    ]
+@app.post('/predict')
+def predict(file: UploadFile = File(...)):
 
-    result = prediction
-    classify = classes[np.argmax(prediction)]
-    confidence = np.max(prediction)
-    st.success(f"Your Class: {classify}")
-    st.success(f"Prediction: {result}")
-    st.write(f"Confidence: {confidence:.2f}")
+    image = Image.open(file.file).convert('RGB')
 
+    processed_image = Preprocess(image)
 
-test_df = pd.read_parquet("DataSet/test.parquet")
+    prediction = model.predict(processed_image)
+
+    predicted_class = predic_class[np.argmax(prediction)]
+    confidence = float(np.max(prediction))
+
+    return JSONResponse(status_code=200, content= {"prediction": predicted_class, "confidence": confidence})
+    
 
